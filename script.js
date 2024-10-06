8d20a0a6fa04ec07caf2e621861057623d765584
Harahel Guilherme <148212477+HarahelGuilherme@users.noreply.github.com>
2024-10-06 15:37:29 -0300

    Música

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function tocarNota(nota) {
     const audio = document.getElementById(nota);
     if (audio) {
-        audio.currentTime = 0; // Reinicia o áudio
-        audio.play(); // Toca o áudio
+        audio.currentTime = 0; // Reinicia o som para tocar do início
+        audio.play(); // Reproduz a nota
     }
 }
